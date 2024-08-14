0e4bccf6d72310db7fb6f7afea25627231c10629
lstiles1 <75706206+lstiles1@users.noreply.github.com>
2024-08-14 00:41:30 -0700

    Update Topbar.tsx

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -9,7 +9,7 @@ function Topbar() {
         <nav className="topbar">
             <Link href="/" className='flex items-center gap-4'>
                 <Image src="/assets/star-logo.png" alt="logo" width={28} height={28} />
-                <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+                <p className='text-heading3-bold text-light-1 max-xs:hidden'>Super Fan Social</p>
             </Link>
 
             <div className='flex items-center gap-1'>
